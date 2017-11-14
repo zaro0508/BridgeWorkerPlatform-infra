dcb73dd17b1a74a565728c982f3d1ad5e1d3806f
Khai Do <zaro0508@gmail.com>
2017-11-14 09:29:27 -0800

    Automate Bridge worker platform infra
    Initial commit to automate the Bridge worker platform app's
    infrastructure.

---
+++ update_cf_stack.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# double interpolate vars from travis
+eval export "AwsSnsNotificationEndpoint=\$AwsSnsNotificationEndpoint_$TRAVIS_BRANCH"
+eval export "BridgeEnv=\$BridgeEnv_$TRAVIS_BRANCH"
+eval export "BridgeWorkerEmail=\$BridgeWorkerEmail_$TRAVIS_BRANCH"
+eval export "BridgeWorkerStudy=\$BridgeWorkerStudy_$TRAVIS_BRANCH"
+eval export "BridgeWorkerPassword=\$BridgeWorkerPassword_$TRAVIS_BRANCH"
+eval export "Env=\$env_$TRAVIS_BRANCH"
+eval export "JbdcConnectionString=\$JbdcConnectionString_$TRAVIS_BRANCH"
+eval export "SynapseApiKey=\$SynapseApiKey_$TRAVIS_BRANCH"
+eval export "SynapseUser=\$SynapseUser_$TRAVIS_BRANCH"
+
+# deploy with evaluated vars
+aws cloudformation update-stack \
+--stack-name $STACK_NAME \
+--capabilities CAPABILITY_NAMED_IAM \
+--template-body file://cf_templates/eb_app.yml \
+--parameters \
+ParameterKey=AwsSnsNotificationEndpoint,ParameterValue=$AwsSnsNotificationEndpoint \
+ParameterKey=AwsDefaultVpcId,ParameterValue=$AwsDefaultVpcId \
+ParameterKey=BridgeEnv,ParameterValue=$BridgeEnv \
+ParameterKey=BridgeUser,ParameterValue=$BridgeUser \
+ParameterKey=BridgeWorkerEmail,ParameterValue="$BridgeWorkerEmail" \
+ParameterKey=BridgeWorkerStudy,ParameterValue=$BridgeWorkerStudy \
+ParameterKey=BridgeWorkerPassword,ParameterValue=$BridgeWorkerPassword \
+ParameterKey=EC2InstanceType,ParameterValue=t2.micro \
+ParameterKey=Env,ParameterValue=$env \
+ParameterKey=JbdcConnectionString,ParameterValue=$JbdcConnectionString \
+ParameterKey=AwsSolutionStackName,ParameterValue=$AwsSolutionStackName \
+ParameterKey=SynapseApiKey,ParameterValue=$SynapseApiKey \
+ParameterKey=SynapseUser,ParameterValue=$SynapseUser
