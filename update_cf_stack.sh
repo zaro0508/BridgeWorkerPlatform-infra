f11184a34cbf4c2f2f202a7cd74ea9b35c12ea69
Khai Do <zaro0508@gmail.com>
2017-11-14 16:59:58 -0800

    fix more typos

--- update_cf_stack.sh
+++ update_cf_stack.sh
@@ -6,7 +6,7 @@ eval export "BridgeEnv=\$BridgeEnv_$TRAVIS_BRANCH"
 eval export "BridgeWorkerEmail=\$BridgeWorkerEmail_$TRAVIS_BRANCH"
 eval export "BridgeWorkerStudy=\$BridgeWorkerStudy_$TRAVIS_BRANCH"
 eval export "BridgeWorkerPassword=\$BridgeWorkerPassword_$TRAVIS_BRANCH"
-eval export "Env=\$env_$TRAVIS_BRANCH"
+eval export "Env=\$Env_$TRAVIS_BRANCH"
 eval export "JbdcConnectionString=\$JbdcConnectionString_$TRAVIS_BRANCH"
 eval export "SynapseApiKey=\$SynapseApiKey_$TRAVIS_BRANCH"
 eval export "SynapseUser=\$SynapseUser_$TRAVIS_BRANCH"
@@ -21,12 +21,12 @@ ParameterKey=AwsSnsNotificationEndpoint,ParameterValue=$AwsSnsNotificationEndpoi
 ParameterKey=AwsDefaultVpcId,ParameterValue=$AwsDefaultVpcId \
 ParameterKey=BridgeEnv,ParameterValue=$BridgeEnv \
 ParameterKey=BridgeUser,ParameterValue=$BridgeUser \
-ParameterKey=BridgeWorkerEmail,ParameterValue="$BridgeWorkerEmail" \
+ParameterKey=BridgeWorkerEmail,ParameterValue=$BridgeWorkerEmail \
 ParameterKey=BridgeWorkerStudy,ParameterValue=$BridgeWorkerStudy \
 ParameterKey=BridgeWorkerPassword,ParameterValue=$BridgeWorkerPassword \
 ParameterKey=EC2InstanceType,ParameterValue=t2.micro \
 ParameterKey=Env,ParameterValue=$Env \
 ParameterKey=JbdcConnectionString,ParameterValue=$JbdcConnectionString \
-ParameterKey=AwsSolutionStackName,ParameterValue=$AwsSolutionStackName \
+ParameterKey=AwsSolutionStackName,ParameterValue="$AwsSolutionStackName" \
 ParameterKey=SynapseApiKey,ParameterValue=$SynapseApiKey \
 ParameterKey=SynapseUser,ParameterValue=$SynapseUser
