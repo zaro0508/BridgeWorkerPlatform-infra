642f073e81a7af073300f270b1ca2f10391ac821
Khai Do <zaro0508@gmail.com>
2018-06-09 15:33:02 -0700

    Re-enable scepter to deploy CF templates
    The current build scripts are not longer supported due to
    underlying Bridge infra changes.  Use sceptre to deploy
    CF templates going forward.

--- setup_aws_cli.sh
+++ setup_aws_cli.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 # double interpolate vars from travis
+eval export "AwsCfServiceRoleArn=\$AwsCfServiceRoleArn_$TRAVIS_BRANCH"
 eval export "AwsTravisAccessKey=\$AwsTravisAccessKey_$TRAVIS_BRANCH"
 eval export "AwsTravisSecretAccessKey=\$AwsTravisSecretAccessKey_$TRAVIS_BRANCH"
 
 pip install --upgrade awscli
 mkdir ~/.aws
-echo -e "[default]\nregion=us-east-1" > ~/.aws/config
-echo -e "[default]\naws_access_key_id=$AwsTravisAccessKey\naws_secret_access_key=$AwsTravisSecretAccessKey" > ~/.aws/credentials
+echo -e "[default]\nregion=us-east-1\nsource_profile=default\nrole_arn=$AwsCfServiceRoleArn" > ~/.aws/config
+echo -e "[default]\nregion=us-east-1\naws_access_key_id=$AwsTravisAccessKey\naws_secret_access_key=$AwsTravisSecretAccessKey" > ~/.aws/credentials
+
